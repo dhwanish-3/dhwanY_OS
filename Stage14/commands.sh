33c632c6e88b1abe46e6329991ccb96db4578c02
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-08-31 00:23:51 +0530

    Stage 14

---
+++ Stage14/commands.sh
@@ -0,0 +1,8 @@
+load --os $HOME/myexpos/Stage13/os_startup.xsm
+load --init $HOME/myexpos/Stage14/init.xsm
+load --idle $HOME/myexpos/Stage14/idle.xsm
+load --exec $HOME/myexpos/Stage14/even.xsm
+load --int=10 $HOME/myexpos/Stage14/int10.xsm
+load --module 5 $HOME/myexpos/Stage14/module_5.xsm
+load --module 7 $HOME/myexpos/Stage14/module_7.xsm
+load --int=timer $HOME/myexpos/Stage14/timer.xsm
