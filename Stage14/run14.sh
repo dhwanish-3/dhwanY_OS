f6a70cf15952a9efdb86a0ee35d0b438cd937aea
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-08-31 11:08:14 +0530

    Stage 14 Assgn1 complete

--- Stage14/run14.sh
+++ Stage14/run14.sh
@@ -1,8 +1,10 @@
 load --os $HOME/myexpos/Stage13/os_startup.xsm
 load --init $HOME/myexpos/Stage14/init.xsm
 load --idle $HOME/myexpos/Stage14/idle.xsm
+rm even.xsm
 load --exec $HOME/myexpos/Stage14/even.xsm
 load --int=10 $HOME/myexpos/Stage14/int10.xsm
+load --int=7 $HOME/myexpos/Stage10/int7.xsm
 load --module 5 $HOME/myexpos/Stage14/module_5.xsm
 load --module 7 $HOME/myexpos/Stage14/module_7.xsm
 load --int=timer $HOME/myexpos/Stage14/timer.xsm
