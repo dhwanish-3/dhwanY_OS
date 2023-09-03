9d6554bd45b5057739815b0f930522635fce3c48
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-09-03 13:01:39 +0530

    Stage16 done with it

---
+++ xfs-interface/run16.sh
@@ -0,0 +1,14 @@
+load --os $HOME/myexpos/Stage13/os_startup.xsm
+load --init $HOME/myexpos/Stage16/init.xsm
+load --idle $HOME/myexpos/Stage14/idle.xsm
+load --int=10 $HOME/myexpos/Stage14/int10.xsm
+load --exhandler $HOME/myexpos/Stage6/Assignment/haltprog.xsm
+load --int=7 $HOME/myexpos/Stage15/int7.xsm
+load --int=6 $HOME/myexpos/Stage16/int6.xsm
+load --int=console $HOME/myexpos/Stage16/console_int.xsm
+load --module 5 $HOME/myexpos/Stage14/module_5.xsm
+load --module 7 $HOME/myexpos/Stage16/module_7.xsm
+load --module 0 $HOME/myexpos/Stage15/module_0.xsm
+load --module 4 $HOME/myexpos/Stage16/module_4.xsm
+load --int=timer $HOME/myexpos/Stage14/timer.xsm
+load --library $HOME/myexpos/expl/library.lib
