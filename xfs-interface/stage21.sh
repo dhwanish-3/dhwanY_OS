8fe6e2c962d06879222ceba4d33e4f70a842d983
dhwanish-3 <108540722+dhwanish-3@users.noreply.github.com>
2023-10-08 14:58:20 +0530

    Stage21 + 2 Assgn's complete

---
+++ xfs-interface/stage21.sh
@@ -0,0 +1,29 @@
+load --os $HOME/myexpos/Stage13/os_startup.xsm
+load --init $HOME/myexpos/Stage21/Assgn1/shell2.xsm
+load --idle $HOME/myexpos/Stage14/idle.xsm
+load --int=10 $HOME/myexpos/Stage20/exit.xsm
+load --int=11 $HOME/myexpos/Stage21/int11.xsm
+load --int=15 $HOME/myexpos/Stage21/int15.xsm
+load --int=9 $HOME/myexpos/Stage19/int9.xsm
+load --int=8 $HOME/myexpos/Stage20/fork.xsm
+load --exhandler $HOME/myexpos/Stage21/exhandler.xsm
+load --int=7 $HOME/myexpos/Stage15/int7.xsm
+load --int=6 $HOME/myexpos/Stage16/int6.xsm
+load --int=disk $HOME/myexpos/Stage18/int2.xsm
+load --int=console $HOME/myexpos/Stage16/console_int.xsm
+load --module 5 $HOME/myexpos/Stage20/scheduler.xsm
+load --module 7 $HOME/myexpos/Stage21/mod7.xsm
+load --module 0 $HOME/myexpos/Stage18/module_0.xsm
+load --module 1 $HOME/myexpos/Stage21/mod1.xsm
+load --module 2 $HOME/myexpos/Stage19/module_2.xsm
+load --module 4 $HOME/myexpos/Stage18/module_4.xsm
+load --int=timer $HOME/myexpos/Stage14/timer.xsm
+load --library $HOME/myexpos/expl/library.lib
+rm exec.xsm
+load --exec $HOME/myexpos/Stage19/exec.xsm
+rm list.xsm
+load --exec $HOME/myexpos/Stage19/Assgn1/list.xsm
+rm odd.xsm
+load --exec $HOME/myexpos/Stage20/Assgn1/odd.xsm
+rm even.xsm
+load --exec $HOME/myexpos/Stage20/Assgn1/even.xsm
